2cf4a81eda175a759a03576cc6abd85879fcc1da
HolyCloudDevOps <hollydevops@gmail.com>
2025-01-06 22:39:17 -0500

    Tested terraform feature lifecycle with taints and aws terraform feature of count for multiple instances etc

---
+++ lab-6/user_data.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+apt update -y
+apt install apache2 -y
+
+MYIP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+
+echo "<h2>Webserver with Private IP: $MYIP
+Static IP test
+</h2> <br> new Created by TF and me new" > /var/www/html/index.html
+
+systemctl start apache2
+systemctl enable apache2
